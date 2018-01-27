c1bf9a427a3b30a39e0f59756247b018caac293e
Wyrwinio <33356058+Wyrwinio@users.noreply.github.com>
2018-01-27 16:09:32 +0100

    Add files via upload

---
+++ Mors okienkowy/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Mors_okienkowy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.sourceList = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button3 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.customWaveViewer1 = new Mors_okienkowy.CustomWaveViewer();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(379, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tłumacz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.tlumaczenienamorsa);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(53, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Tłumacz i zapisz do .wav";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.tlumaczenienamorsapluszapis);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(217, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(147, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(217, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(147, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // sourceList
+            // 
+            this.sourceList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.sourceList.Location = new System.Drawing.Point(517, 30);
+            this.sourceList.MultiSelect = false;
+            this.sourceList.Name = "sourceList";
+            this.sourceList.Size = new System.Drawing.Size(235, 235);
+            this.sourceList.TabIndex = 4;
+            this.sourceList.UseCompatibleStateImageBehavior = false;
+            this.sourceList.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Device";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Channels";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(594, 271);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Odśwież źródła";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.OdswiezZrodla);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(237, 119);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(108, 42);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Otwórz i rozszyfruj plik .wav";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(234, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Treść do zaszyfrowania";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(243, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Treść zaszyfrowana";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(217, 201);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(147, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(242, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Treść odszyfrowana";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(319, 271);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Wykres fal pliku";
+            this.label4.Visible = false;
+            // 
+            // customWaveViewer1
+            // 
+            this.customWaveViewer1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.customWaveViewer1.Location = new System.Drawing.Point(0, 294);
+            this.customWaveViewer1.Name = "customWaveViewer1";
+            this.customWaveViewer1.PenColor = System.Drawing.Color.DodgerBlue;
+            this.customWaveViewer1.PenWidth = 1F;
+            this.customWaveViewer1.SamplesPerPixel = 128;
+            this.customWaveViewer1.Size = new System.Drawing.Size(796, 341);
+            this.customWaveViewer1.StartPosition = ((long)(0));
+            this.customWaveViewer1.TabIndex = 9;
+            this.customWaveViewer1.Visible = false;
+            this.customWaveViewer1.WaveStream = null;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(591, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Źródła dźwięku";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 635);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.customWaveViewer1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.sourceList);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListView sourceList;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button6;
+        public CustomWaveViewer customWaveViewer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Mors okienkowy/Form1.cs
@@ -0,0 +1,454 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mors_okienkowy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        NAudio.Wave.WaveIn sourceStream = null;
+        NAudio.Wave.DirectSoundOut waveOut = null;
+        NAudio.Wave.WaveFileWriter waveWriter = null;
+        private async void tlumaczenienamorsa(object sender, EventArgs e)
+        {
+            textBox2.Text = null;
+            char[] tablica = null;
+            String input = textBox1.Text;
+            input = input.ToUpper();
+            Dictionary<char, String> morse = new Dictionary<char, String>()
+            {
+                {'A' , ".-"},
+                {'Ą' , ".-.-"},
+                {'B' , "-..."},
+                {'C' , "-.-."},
+                {'Ć' , "-.-.."},
+                {'D' , "-.."},
+                {'E' , "."},
+                {'Ę' , "..-.."},
+                {'F' , "..-."},
+                {'G' , "--."},
+                {'H' , "...."},
+                {'I' , ".."},
+                {'J' , ".---"},
+                {'K' , "-.-"},
+                {'L' , ".-.."},
+                {'Ł' , ".-..-"},
+                {'M' , "--"},
+                {'N' , "-."},
+                {'Ń' , "--.--"},
+                {'O' , "---"},
+                {'Ó' , "---."},
+                {'P' , ".--."},
+                {'Q' , "--.-"},
+                {'R' , ".-."},
+                {'S' , "..."},
+                {'Ś' , "...-..."},
+                {'T' , "-"},
+                {'U' , "..-"},
+                {'V' , "...-"},
+                {'W' , ".--"},
+                {'X' , "-..-"},
+                {'Y' , "-.--"},
+                {'Z' , "--.."},
+                {'Ż' , "--..-."},
+                {'Ź' , "--..-"},
+                {'0' , "-----"},
+                {'1' , ".----"},
+                {'2' , "..---"},
+                {'3' , "...--"},
+                {'4' , "....-"},
+                {'5' , "....."},
+                {'6' , "-...."},
+                {'7' , "--..."},
+                {'8' , "---.."},
+                {'9' , "----."},
+
+            };
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if (i > 0)
+                {
+                    textBox2.Text += "/";
+                    await Task.Delay(2000);                  
+                }
+                if (input[i] == ' ')
+                {
+                    await Task.Delay(5000);
+                }
+                else
+                {
+                    char c = input[i];
+                    if (morse.ContainsKey(c))
+                        textBox2.Text += morse[c];
+                    tablica = morse[c].ToCharArray();                  
+                    for (int i2 = 0; i2 < tablica.Length; i2++)
+                    {
+                        if (tablica[i2] == '.')
+                        {
+
+                            Action beep = () => Console.Beep(1000, 600); await Task.Delay(2000); beep.BeginInvoke(null, null);                           
+
+                        }
+                        if (tablica[i2] == '-')
+                        {
+
+                            Action beep2 = () => Console.Beep(1000, 1000); await Task.Delay(2000); beep2.BeginInvoke(null, null);                      
+                        }
+                    }
+
+
+                }
+            }
+        }
+
+        private async void tlumaczenienamorsapluszapis(object sender, EventArgs e)
+        {
+            textBox2.Text = null;
+            if (sourceList.SelectedItems.Count == 0)
+            {            
+                MessageBox.Show("Wybierz źródło nagrywania"); }
+            else {              
+
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Wave File (*.wav)|*.wav;";
+            if (save.ShowDialog() != System.Windows.Forms.DialogResult.OK) return;
+
+            int deviceNumber = sourceList.SelectedItems[0].Index;
+
+            sourceStream = new NAudio.Wave.WaveIn();
+            sourceStream.DeviceNumber = deviceNumber;
+            sourceStream.WaveFormat = new NAudio.Wave.WaveFormat(44100, NAudio.Wave.WaveIn.GetCapabilities(deviceNumber).Channels);
+            sourceStream.DataAvailable += new EventHandler<NAudio.Wave.WaveInEventArgs>(sourceStream_DataAvailable);
+            waveWriter = new NAudio.Wave.WaveFileWriter(save.FileName, sourceStream.WaveFormat);
+            sourceStream.StartRecording();
+            char[] tablica = null;
+            String input = textBox1.Text;
+            input = input.ToUpper();
+            Dictionary<char, String> morse = new Dictionary<char, String>()
+            {
+                {'A' , ".-"},
+                {'Ą' , ".-.-"},
+                {'B' , "-..."},
+                {'C' , "-.-."},
+                {'Ć' , "-.-.."},
+                {'D' , "-.."},
+                {'E' , "."},
+                {'Ę' , "..-.."},
+                {'F' , "..-."},
+                {'G' , "--."},
+                {'H' , "...."},
+                {'I' , ".."},
+                {'J' , ".---"},
+                {'K' , "-.-"},
+                {'L' , ".-.."},
+                {'Ł' , ".-..-"},
+                {'M' , "--"},
+                {'N' , "-."},
+                {'Ń' , "--.--"},
+                {'O' , "---"},
+                {'Ó' , "---."},
+                {'P' , ".--."},
+                {'Q' , "--.-"},
+                {'R' , ".-."},
+                {'S' , "..."},
+                {'Ś' , "...-..."},
+                {'T' , "-"},
+                {'U' , "..-"},
+                {'V' , "...-"},
+                {'W' , ".--"},
+                {'X' , "-..-"},
+                {'Y' , "-.--"},
+                {'Z' , "--.."},
+                {'Ż' , "--..-."},
+                {'Ź' , "--..-"},
+                {'0' , "-----"},
+                {'1' , ".----"},
+                {'2' , "..---"},
+                {'3' , "...--"},
+                {'4' , "....-"},
+                {'5' , "....."},
+                {'6' , "-...."},
+                {'7' , "--..."},
+                {'8' , "---.."},
+                {'9' , "----."},
+            };
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if (i > 0)
+                {
+                    textBox2.Text += "/";
+                    await Task.Delay(2000);
+                
+                }
+                if (input[i] == ' ')
+                {
+                    await Task.Delay(5000);
+                }
+                else
+                {
+                    char c = input[i];
+                    if (morse.ContainsKey(c))
+                        textBox2.Text += morse[c];
+                    tablica = morse[c].ToCharArray();
+
+
+                    
+                    for (int i2 = 0; i2 < tablica.Length; i2++)
+                    {
+                        if (tablica[i2] == '.')
+                        {
+
+                           
+                            Action beep = () => Console.Beep(1000, 600); await Task.Delay(2000); beep.BeginInvoke(null, null);
+                            
+                          
+
+
+                        }
+                        if (tablica[i2] == '-')
+                        {
+
+                            Action beep2 = () => Console.Beep(1000, 1000); await Task.Delay(2000); beep2.BeginInvoke(null, null);
+                            
+
+                           
+                        }
+                    }
+
+
+                }
+            }
+
+
+
+            if (waveOut != null)
+            {
+                await Task.Delay(1000);
+                waveOut.Stop();
+                waveOut.Dispose();
+                waveOut = null;
+            }
+            if (sourceStream != null)
+            {
+                await Task.Delay(1000);
+                sourceStream.StopRecording();
+                sourceStream.Dispose();
+                sourceStream = null;
+            }
+            if (waveWriter != null)
+            {
+                await Task.Delay(1000);
+                waveWriter.Dispose();
+                waveWriter = null;
+            }
+
+         
+
+        }
+        }
+        private void sourceStream_DataAvailable(object sender, NAudio.Wave.WaveInEventArgs e)
+        {
+            if (waveWriter == null) return;
+
+            waveWriter.WriteData(e.Buffer, 0, e.BytesRecorded);
+            waveWriter.Flush();
+        }
+
+        private void OdswiezZrodla(object sender, EventArgs e)
+        {
+            
+                List<NAudio.Wave.WaveInCapabilities> sources = new List<NAudio.Wave.WaveInCapabilities>();
+
+                for (int i = 0; i < NAudio.Wave.WaveIn.DeviceCount; i++)
+                {
+                    sources.Add(NAudio.Wave.WaveIn.GetCapabilities(i));
+                }
+
+                sourceList.Items.Clear();
+
+                foreach (var source in sources)
+                {
+                    ListViewItem item = new ListViewItem(source.ProductName);
+                    item.SubItems.Add(new ListViewItem.ListViewSubItem(item, source.Channels.ToString()));
+                    sourceList.Items.Add(item);
+                }
+            
+        }     
+              
+        private NAudio.Wave.WaveFileReader wave = null;
+        private NAudio.Wave.DirectSoundOut output = null;
+        private void button6_Click(object sender, EventArgs e)
+        
+            {
+            textBox2.Text = null;
+            Dictionary<String, char> morse2 = new Dictionary<String, char>()
+            {
+                {".-" , 'A'},
+                {".-.-" ,'Ą'},
+                {"-...", 'B'},
+                {"-.-.",'C' },
+                {"-.-..",'Ć'},
+                {"-.." , 'D'},
+                {"." , 'E'},
+                {"..-.." , 'Ę'},
+                {"..-." , 'F'},
+                { "--.", 'G'},
+                { "....", 'H'},
+                {".." , 'I'},
+                {".---" , 'J'},
+                {"-.-" , 'K'},
+                {".-.." , 'L'},
+                { ".-..-", 'Ł'},
+                { "--", 'M'},
+                {"-." ,'N' },
+                { "--.--", 'Ń'},
+                { "---", 'O'},
+                {"---." , 'Ó'},
+                { ".--.", 'P'},
+                { "--.-", 'Q'},
+                { ".-.", 'R'},
+                { "...", 'S'},
+                { "...-...", 'Ś'},
+                { "-", 'T'},
+                { "..-", 'U'},
+                {"...-" , 'V'},
+                {".--" ,'W' },
+                { "-..-", 'X'},
+                { "-.--", 'Y'},
+                {"--.." , 'Z'},
+                { "--..-.", 'Ż'},
+                {"--..-" , 'Ź'},
+                { "-----", '0'},
+                {".----" ,'1' },
+                {"..---" , '2'},
+                { "...--", '3'},
+                {"....-" , '4'},
+                {"....." ,'5' },
+                {"-...." , '6'},
+                { "--...", '7'},
+                {"---.." , '8'},
+                {"----." , '9'},
+
+            };
+            OpenFileDialog open = new OpenFileDialog();
+                open.Filter = "Wave File (*.wav)|*.wav;";
+                if (open.ShowDialog() != DialogResult.OK) return;
+                DisposeWave();
+                wave = new NAudio.Wave.WaveFileReader(open.FileName);
+            customWaveViewer1.WaveStream = new NAudio.Wave.WaveFileReader(open.FileName);
+            AudioFileReader readertest = new AudioFileReader(open.FileName);
+            customWaveViewer1.Visible = true;
+            label4.Visible = true;
+            int bytesnumber = (int)readertest.Length;
+            var buffer = new float[bytesnumber];
+            readertest.Read(buffer, 0, bytesnumber);
+            String xxx = null;
+            String odszyfrowane = null;
+            int i = 0;
+            int poczatekbipa = 0;
+            int koniecbipa = 0;
+            while (i < buffer.Length - 1)
+            {
+                while (buffer[i] < 0.2 && buffer[i] > -0.2 && i< buffer.Length - 1) 
+                {
+                    i++;
+                }            
+            poczatekbipa = i;
+                while (buffer[i] != 0 && i< buffer.Length - 1) 
+                {
+                    i++;
+                }
+                if(poczatekbipa - koniecbipa > 176500 && poczatekbipa - koniecbipa < 530000 && koniecbipa != 0){
+                    odszyfrowane = odszyfrowane + '/';                    
+                }
+                if (poczatekbipa - koniecbipa > 530000)
+                {
+                    odszyfrowane = odszyfrowane + "//";
+                }
+            
+            koniecbipa = i;
+                if(koniecbipa - poczatekbipa > 21180)
+                {
+                    Console.WriteLine("Poczatek: " + poczatekbipa + " Koniec:" + koniecbipa);
+                    int dlugoscbipa = koniecbipa - poczatekbipa;
+                    Console.WriteLine("Długość bipa w probkach= " + dlugoscbipa);
+                    int dlugoscbipawsek = dlugoscbipa / 353;
+                    Console.WriteLine("Długość bipa w sekundach= " + dlugoscbipawsek);
+                    if (dlugoscbipawsek < 130)
+                    {
+                        Console.WriteLine("Bip krótki ");
+                        odszyfrowane = odszyfrowane + '.';
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bip długi");
+                        odszyfrowane = odszyfrowane + '-';
+                    }
+                }
+            }
+            Console.Write("Odszyfrowana zawartosc to: " + odszyfrowane);
+            char[] tablica2 = null;
+            tablica2 = odszyfrowane.ToCharArray();
+            Console.WriteLine();
+            string slowo = null;
+            string odszyfrowanawiadomosc = null;
+            for (int x = 0; x < odszyfrowane.Length; x++)
+            {
+                if (tablica2[x] != '/')
+                {
+                    slowo = slowo + tablica2[x];
+                }
+                else {
+                    if (slowo == null)
+                    {
+                        odszyfrowanawiadomosc = odszyfrowanawiadomosc + ' '; 
+                    }
+                    else if (morse2.ContainsKey(slowo))
+                    {
+                        odszyfrowanawiadomosc = odszyfrowanawiadomosc + morse2[slowo];
+                    }                    
+                    slowo = null;
+                }                
+            }
+            textBox3.Text = odszyfrowanawiadomosc;
+            Console.WriteLine("Odszyfrowana wiadomość to: " + odszyfrowanawiadomosc);            
+            MessageBox.Show(Convert.ToString(buffer.Length));      
+            output = new NAudio.Wave.DirectSoundOut();
+                output.Init(new NAudio.Wave.WaveChannel32(wave));
+                output.Play();            
+            }
+        private void DisposeWave()
+        {
+            if (output != null)
+            {
+                if (output.PlaybackState == NAudio.Wave.PlaybackState.Playing) output.Stop();
+                output.Dispose();
+                output = null;
+            }
+            if (wave != null)
+            {
+                wave.Dispose();
+                wave = null;
+            }
+        }
+
+       
+    }
+}
